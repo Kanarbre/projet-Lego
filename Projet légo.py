d031dff7f03890263f142698c6f1569ec80ca4c1
CharlotteBORIE <lilou.borie@gmail.com>
2021-03-18 12:42:58 +0100

    update des calculs

--- Projet légo.py
+++ Projet légo.py
@@ -6,15 +6,15 @@ class Table_lego:
     
 	def __init__(self):
         
-		self.tk=Tk()#créer la fenêtre
+		self.tk=Tk()#crÃ©er la fenÃªtre
 		
 		self.canvas=Canvas(self.tk,takefocus='', width=800, height=600, bg='white')
 		self.canvas.grid(column=0, row=0,rowspan=40)
 		
-		#création du catalogue
+		#crÃ©ation du catalogue
 		self.catalogue=self.get_catalogue()
 		
-		#frame du matériel
+		#frame du matÃ©riel
 		
 		self.matFrame=Frame(self.tk,borderwidth=2,relief='groove')
 		self.matFrame.grid(column=1,row=0)
@@ -22,7 +22,7 @@ class Table_lego:
 		self.matcan=Canvas(self.matFrame,width=100, height=600)
 		self.matcan.grid()
 		
-		self.liste=Label(self.matcan, text="Matériel nécessaire")
+		self.liste=Label(self.matcan, text="MatÃ©riel nÃ©cessaire")
 		self.liste.grid()
 		
 		self.plaque_lab=Label(self.matcan,text="")
@@ -40,7 +40,7 @@ class Table_lego:
 		self.titre=Label(self.mainFrame, text= 'MA TABLE LEGO', font='Helvetica', foreground='red')
 		self.titre.grid()              
 		
-		"""Création de la fenêtre de demande des dimensions"""
+		"""CrÃ©ation de la fenÃªtre de demande des dimensions"""
 		
 		self.question = Label(self.mainFrame, text="Quel est la taille de la table ?(longueur,largeur)")
 		
@@ -57,8 +57,7 @@ class Table_lego:
 		self.taille_but.grid()
 		
 		#dessin de la table et de la plaque
-		self.x=None
-		self.y=None
+		self.table=None
 		self.L=[]
 		
 		self.tk.mainloop()
@@ -66,60 +65,60 @@ class Table_lego:
 		
 		
 	def taille(self):
-		""""effacer le canvas précédent"""
-		self.canvas.delete(self.x)
-		self.canvas.delete(self.y)
+		""""effacer le canvas prÃ©cÃ©dent"""
+		self.canvas.delete(self.table)
 		for elem in self.L:
 			self.canvas.delete(elem)
 		self.L=[]
 		
 		"""calculer les dimensions"""
 		
-		#échelle 32/25 tenons/cm
+		echelle=32/25
+		#Ã©chelle 32/25 tenons/cm
 		#4 pixels par tenons
+		pixel=4
 		
 		lon,lar=self.dimensions.get().split(',')
-		lon_table=int(lon)*100/32 #nombre de tenons sur la longueur
-		lar_table=int(lar)*100/32
-		lon_plaque=int(int(lon)*128/25-(int(lon)*128%25))
-		lar_plaque=int(int(lar)*128/25-(int(lar)*128%25))
-		print(lon_plaque,lon_table)
+		lon_table=int(int(lon)*echelle) #nombre de tenons sur la longueur
+		lar_table=int(int(lar)*echelle)
+		lon_plaque=ceil(lon_table/32) #nombre de plaque
+		lar_plaque=ceil(lar_table/32)
+	
 		
-		# objet nécessaire
-		nombre_plaque=ceil((lon_plaque*lar_plaque)/16384)
+		# objet nÃ©cessaire
+		nombre_plaque=max(lon_plaque,lar_plaque)
+		
 		
 		self.plaque_lab.destroy()
-		if nombre_plaque!=lon_plaque*lar_plaque/16384:
+		if lon_table%32>1 or lar_table%32>1:
 			self.plaque_lab=Label(self.matcan,text='x'+str(nombre_plaque)+'\n plaque de base \n DECOUPEZ LA PLAQUE')
 		else:
 			self.plaque_lab=Label(self.matcan,text='x'+str(nombre_plaque)+'\n plaque de base')
 		self.plaque_lab.grid()
 		
-		self.set_img(self.canvas,'photo plaque légo.png')
+		self.set_img(self.canvas,'photo plaque lÃ©go.png')
+
+
+		"""dessiner la plaque"""
 		
-		#dessiner la plaque
+		#calcul en pixel
+		lo=lon_table*pixel
+		la=lar_table*pixel
 		
-		"""Nombre de tenons"""
-		lo=lon_plaque//4
-		la=lar_plaque//4
-	
+		"""crÃ©er la table"""
+		self.table=self.canvas.create_rectangle(350-lo,300-la,350+lo,300+la,fill="green")
+		self.table
 		
-		"""créer la table"""
-		self.x=self.canvas.create_rectangle(350-lon_table,300-lar_table,350+lon_table,300+lar_table)
-		self.x
-		self.y=self.canvas.create_rectangle(350-lon_plaque,300-lar_plaque,350+lon_plaque,300+lar_plaque,fill='green')
-		self.y
-		
-		"""crééer les tenons"""
-		a=350-lon_plaque+4
-		for i in range (lo):
-			b=300-lar_plaque+4
-			for j in range (la):
-				point=self.canvas.create_oval(a-3,b-3,a+3,b+3)
+		"""crÃ©Ã©er les tenons"""
+		a=350-lo+pixel
+		for i in range (lon_table):
+			b=300-la+pixel
+			for j in range (lar_table):
+				point=self.canvas.create_oval(a-(pixel-1),b-(pixel-1),a+(pixel-1),b+(pixel-1))
 				self.L.append(point)
 				point
-				b+=8
-			a+=8
+				b+=pixel*2
+			a+=pixel*2
 		
 		
 
@@ -128,24 +127,17 @@ class Table_lego:
 		can.create_image(795,50,image=self.img)
 		
 	def get_catalogue(self):
-		couleurs=["noir","rouge","bleu clair","bleu foncé","jaune","orange","vert foncé","vert clair","violet","marron","blanc","gris"]
-		formes=[["carré ",2,2],["rectangle horizontal ",2,4],["rectangle vertical ",4,2]]
+		couleurs=["noir","rouge","bleu clair","bleu foncÃ©","jaune","orange","vert foncÃ©","vert clair","violet","marron","blanc","gris"]
+		formes=[["carrÃ© ",2,2],["rectangle horizontal ",2,4],["rectangle vertical ",4,2]]
 		cata=[[0,"plaque verte",32,32,"vert",7.99]]
 		i=0
 		while i<len(couleurs)*len(formes):
 			couleur=couleurs[i//3]
-			forme=formes.copy()[i%3];
+			forme=formes.copy()[i%3]
 			elem=[i+1]+forme+[couleur]+[0.20]
 			elem[1]+=couleur
 			cata.append(elem)
 			i+=1
 		return cata
-			
-			
-		
-	
-		
 
-        
 Table_lego()
-        
